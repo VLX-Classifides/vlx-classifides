2463e26b9d50725ca3c1601c1b8eec736bf2dded
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-09-14 22:28:50 +0530

    header modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import * as actionTypes from "../../store/actions/actionTypes";
+import $ from "jquery";
 
 class Header extends Component {
   state = {
     user: JSON.parse(localStorage.getItem("user")),
   };
+  componentWillMount() {
+    if (this.state.user) {
+      this.props.authenticate();
+    }
+    // $(document).ready(function () {
+    //   $(".dropdown-toggle").dropdown();
+    // });
+  }
   render() {
     console.log(this.props);
     return (
@@ -41,11 +51,13 @@ class Header extends Component {
                   Analytics
                 </a>
               </li> */}
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/ads">
-                  Advertisements
-                </Link>
-              </li>
+              {this.state.user.role === "seller" && (
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/ads">
+                    Advertisements
+                  </Link>
+                </li>
+              )}
               <li className="nav-item">
                 <Link to="/checkout" className="nav-link text-white">
                   <i class="fa fa-lg fa-shopping-cart"></i>
@@ -71,7 +83,31 @@ class Header extends Component {
                   </li>
                 </>
               ) : (
-                <li className="nav-item mx-1">
+                <li className="nav-item mx-1 d-flex align-items-center">
+                  {/* <div className="d-flex align-items-center">
+                    <i
+                      class="fa fa-2x my-auto text-white fa-user-circle display-6 dropdown-toggle"
+                      id="dropdownMenuButton1"
+                      data-toggle="dropdown"
+                      style={{ cursor: "pointer" }}
+                    ></i>
+                    <div
+                      className="dropdown-menu px-2"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <Link to="/profile">
+                        <p>Profile</p>
+                      </Link>
+                      <div
+                        onClick={this.toggleLogout}
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      >
+                        <p>Logout</p>
+                      </div>
+                    </div>
+                  </div> */}
                   <Link
                     to="/logout"
                     className="nav-link font-weight-bold btn bg-white text-primary px-5"
@@ -94,4 +130,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    authenticate: () =>
+      dispatch({ type: actionTypes.AUTHENTICATE, data: true }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchtoProps)(Header);
