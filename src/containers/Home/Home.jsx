f265d774c302ca5abf91dd592a9f887d42bea4fe
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-10-01 12:44:07 +0530

    revenue model Ui update

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -26,15 +26,12 @@ class Home extends Component {
           </div>
         )}
         <div>
-          <h2 className="mt-5 mb-2 text-start">Electronic Appliances</h2>
           <Products category="electronics" />
         </div>
         <div>
-          <h2 className="mt-5 mb-2 text-start">Clothes for Men and Women</h2>
           <Products category="clothing" />
         </div>
         <div>
-          <h2 className="mt-5 mb-2 text-start">Home Accessories</h2>
           <Products category="accessories" />
         </div>
       </div>
--- src/containers/SellerRevenue/Revenue.js
+++ src/containers/SellerRevenue/Revenue.js
@@ -54,25 +54,26 @@ export class Revenue extends Component {
     axios
       .get(url, {
         headers: { Authorization: `Bearer ${localStorage.getItem("access")}` },
-      }).then((res) => {
-      if (res.data.responseType) {
-        this.setState({
-          data: {
-            labels: res.data.result.listofdates,
-            datasets: [
-              {
-                label: res.data.message,
-                borderColor: "blue",
-                data: res.data.result.noofsell,
-                borderWidth: 1,
-                fill: "start",
-                backgroundColor: "rgba(211,211,211,0.3)",
-              },
-            ],
-          },
-        });
-      }
-    });
+      })
+      .then((res) => {
+        if (res.data.responseType) {
+          this.setState({
+            data: {
+              labels: res.data.result.listofdates,
+              datasets: [
+                {
+                  label: res.data.message,
+                  borderColor: "blue",
+                  data: res.data.result.noofsell,
+                  borderWidth: 1,
+                  fill: "start",
+                  backgroundColor: "rgba(211,211,211,0.3)",
+                },
+              ],
+            },
+          });
+        }
+      });
   };
 
   getYearRevenue = () => {
@@ -81,25 +82,26 @@ export class Revenue extends Component {
     axios
       .get(url, {
         headers: { Authorization: `Bearer ${localStorage.getItem("access")}` },
-      }).then((res) => {
-      if (res.data.responseType) {
-        this.setState({
-          data: {
-            labels: res.data.result.listofdates,
-            datasets: [
-              {
-                label: res.data.message,
-                borderColor: "blue",
-                data: res.data.result.noofsell,
-                borderWidth: 1,
-                fill: "start",
-                backgroundColor: "rgba(211,211,211,0.3)",
-              },
-            ],
-          },
-        });
-      }
-    });
+      })
+      .then((res) => {
+        if (res.data.responseType) {
+          this.setState({
+            data: {
+              labels: res.data.result.listofdates,
+              datasets: [
+                {
+                  label: res.data.message,
+                  borderColor: "blue",
+                  data: res.data.result.noofsell,
+                  borderWidth: 1,
+                  fill: "start",
+                  backgroundColor: "rgba(211,211,211,0.3)",
+                },
+              ],
+            },
+          });
+        }
+      });
   };
 
   componentWillMount() {
@@ -115,7 +117,7 @@ export class Revenue extends Component {
         x: {
           color: "black",
           grid: {
-            color: "rgba(0,0,0,0.5)",
+            color: "rgba(0,0,0,0.2)",
           },
         },
         y: {
@@ -125,7 +127,7 @@ export class Revenue extends Component {
             stepSize: 1,
           },
           grid: {
-            color: "rgba(0,0,0,0.5)",
+            color: "rgba(0,0,0,0.2)",
           },
         },
       },
@@ -137,8 +139,36 @@ export class Revenue extends Component {
         <br />
         <br />
         <h1 className="text-center">Your Sell Revenue</h1>
-        <Line data={this.state.data} options={options} className="card-body" />
-        <br />
+        <div className="d-flex flex-column">
+          <div className="d-flex flex-row justify-content-start mb-3 mt-2">
+            <button
+              onClick={this.getWeekRevenue}
+              className={"btn mx-1 " + this.state.week}
+            >
+              1W
+            </button>{" "}
+            <button
+              onClick={this.getMonthRevenue}
+              className={"btn mx-1 " + this.state.month}
+            >
+              1M
+            </button>{" "}
+            <button
+              onClick={this.getYearRevenue}
+              className={"btn mx-1 " + this.state.year}
+            >
+              1Y
+            </button>
+          </div>
+          <div>
+            <Line
+              data={this.state.data}
+              options={options}
+              className="card-body shadow rounded"
+            />
+          </div>
+        </div>
+        {/* <br />
         <div className="text-center">
           <button onClick={this.getWeekRevenue} className={this.state.week}>
             Last 7 Days Sell
@@ -150,7 +180,7 @@ export class Revenue extends Component {
             Last 1 Year Sell
           </button>
         </div>
-        <br />
+        <br /> */}
       </div>
     );
   }
